a2f7c50cc8eb1b162219f6310019b41ca4442b70
Atul-Ranjan12 <33323817+Atul-Ranjan12@users.noreply.github.com>
2022-04-07 11:54:33 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,25 +1,13 @@
 ### Importing the dependencies and scripts
 import tensorflow as tf
-from helper_functions import num_filters, stage_of_resolution, resolution_of_stage, NCHW_to_NHWC, NHWC_to_NCHW
+from helper_functions import num_filters, stage_of_resolution, resolution_of_stage
 from typing import List
 
 
-## Convolution layer that scales the weights for an equalized learning rate.
-class WSConv2D(tf.keras.layers.Layer):
-    """""
-    Scales the outputs of the Convolutional Layer so that the weights are scaled for an equalized
-    learning rate. In Convolutional Layers, the learning rate for some weights may be more than required
-    for other weights. This layer fixes it.
-    """""
-    def __init__(self, in_channels, filters, kernel_size=3, strides=1, padding="same", gain=2, *args, **kwargs):
-        super(WSConv2D, self).__init__(*args, **kwargs)
-        self.conv = tf.keras.layers.Conv2D(
-            filters=filters, kernel_size=kernel_size, strides=strides, padding=padding
-        )
-        self.scale = (gain/(in_channels * (kernel_size**2))) ** 0.5
+## TODO: Add a class to scale the outputs from the Convolution layer Here
 
-    def call(self, inputs, **kwargs):
-        return self.conv(inputs * self.scale)
+
+## TODO: Implement the Minibatch Standard Deviation Layer here
 
 
 ## Convolution Layer
@@ -37,27 +25,6 @@ def rgb_conv(kernel_size, padding="same", strides=1, filters=3):
                                   kernel_size=kernel_size,
                                   padding=padding,
                                   strides=strides)
-
-
-## The Upscale class
-class Upscale2D(tf.keras.layers.Layer):
-    def __init__(self, factor: int = 2, *args, **kwargs):
-        self.factor = factor
-        super().__init__(*args, **kwargs)
-
-    def call(self, inputs, **kwargs):
-        if self.factor == 1:
-            return inputs
-        x = NHWC_to_NCHW(inputs)
-        s = x.shape
-        x = tf.reshape(x, [-1, s[1], s[2], 1, s[3], 1])
-        x = tf.tile(x, [1, 1, 1, self.factor, 1, self.factor])
-        x = tf.reshape(x, [-1, s[1], s[2] * self.factor, s[3] * self.factor])
-        x = NCHW_to_NHWC(x)
-        return x
-
-    def compute_output_shape(self, input_shape: tf.TensorShape) -> tf.TensorShape:
-        return tf.TensorShape([*input_shape[:-2], *(d.value * 2 for d in input_shape[-2:])])
 
 
 ## Pixel Normalization Layer
@@ -79,7 +46,7 @@ def GeneratorBlock(stage, kernel_size=3, strides=2, padding="same"):
                                             strides=strides, padding=padding,
                                             activation=tf.nn.leaky_relu),
             PixelNorm(),
-            WSConv2D(in_channels=filters, filters=filters, kernel_size=kernel_size),
+            standard_conv(filters=filters, kernel_size=kernel_size),
             PixelNorm()
         ]
     )
@@ -102,9 +69,8 @@ class Generator(tf.keras.models.Sequential):
                 tf.keras.layers.Dense(units=self.filters * 4 * 4),
                 tf.keras.layers.Reshape([4, 4, self.filters]),
                 PixelNorm(),
-                WSConv2D(in_channels=self.filters,
-                         filters=self.filters,
-                         kernel_size=3),
+                standard_conv(filters=self.filters,
+                              kernel_size=3),
                 PixelNorm()
             ]
         )
@@ -114,6 +80,7 @@ class Generator(tf.keras.models.Sequential):
         self.resolution: int = resolution_of_stage(self.stage)
 
     def call(self, inputs, training=None, mask=None):
+        print("def call was called inside the Generator Class")
         x = tf.nn.l2_normalize(inputs, axis=-1) if self.normalize_latents else inputs
         for layer in self.prog_block:
             x = layer(x)
@@ -128,181 +95,30 @@ class Generator(tf.keras.models.Sequential):
               f"Generator now generating images of resolution {self.resolution} x {self.resolution}")
         generator_block = GeneratorBlock(self.stage)
         self.prog_block.append(generator_block)
-
-# Building the generator
-# class Generator(tf.keras.models.Sequential):
-#     def __init__(self, latent_dim=512, normalize_latents=True, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.stage = 0
-#         self.normalize_latents = normalize_latents
-#         self.filters = num_filters(self.stage)
-#         ### Initial block taking 1x1 to 4x4
-#         self.initial_layers = tf.keras.Sequential(
-#             [
-#                 PixelNorm(),
-#                 tf.keras.layers.Dense(units=self.filters * 4 * 4),
-#                 tf.keras.layers.Reshape([4, 4, self.filters]),
-#                 PixelNorm(),
-#                 WSConv2D(in_channels=self.filters,
-#                          filters=self.filters,
-#                          kernel_size=3),
-#                 PixelNorm()
-#             ]
-#         )
-#         ## Initial RGB layer
-#         self.initial_rgb = rgb_conv(kernel_size=1)
-#         self.prog_block = [tf.keras.layers.InputLayer([latent_dim]), self.initial_layers]
-#         self.resolution: int = resolution_of_stage(self.stage)
-#
-#     @staticmethod
-#     def fade_in(upscaled, generated, alpha = 0.5):
-#         return tf.nn.tanh(alpha * generated + (1 - alpha) * upscaled)
-#
-#     def call(self, inputs, training=None, mask=None):
-#         print("def call was called inside the Generator Class")
-#         if self.stage == 0:
-#             x = tf.nn.l2_normalize(inputs, axis=-1) if self.normalize_latents else inputs
-#             for layer in self.prog_block:
-#                 x = layer(x)
-#             input_shape = x.shape
-#             final_out = tf.keras.layers.Conv2D(3, (1, 1), activation=tf.keras.activations.relu, input_shape=input_shape)(x)
-#         else:
-#             # Path A: Upscale -> RGB
-#             x = inputs
-#             x = Upscale2D()(x)
-#             print(f"Upscaled shape: {x.shape}")
-#             input_shape = x.shape
-#             out1 = tf.keras.layers.Conv2D(3, (1, 1), activation=tf.keras.activations.relu, input_shape=input_shape)(x)
-#             # Path B: Pass it through the progressive block -> RGB
-#             y = tf.nn.l2_normalize(inputs, axis=-1) if self.normalize_latents else inputs
-#             for layer in self.prog_block:
-#                 y = layer(y)
-#             input_shape = y.shape
-#             out2 = tf.keras.layers.Conv2D(3, (1, 1), activation=tf.keras.activations.relu, input_shape=input_shape)(y)
-#             final_out = self.fade_in(out1, out2)
-#         return final_out
-#
-#     def grow(self):
-#         self.stage += 1
-#         self.resolution *= 2
-#         print(f"Grow was called inside the Generator: \n"
-#               f"Generator now generating images of resolution {self.resolution} x {self.resolution}")
-#         generator_block = GeneratorBlock(self.stage)
-#         self.prog_block.append(generator_block)
-#         print(f"Prog blocks: {self.prog_block}")
-
-
-# class Generator(tf.keras.models.Sequential):
-#     def __init__(self, latent_dim=512, normalize_latents=True, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.stage = 0
-#         self.normalize_latents = normalize_latents
-#         self.filters = num_filters(self.stage)
-#         ### Initial block taking 1x1 to 4x4
-#         self.initial_layers = tf.keras.Sequential(
-#             [
-#                 PixelNorm(),
-#                 tf.keras.layers.Dense(units=self.filters * 4 * 4),
-#                 tf.keras.layers.Reshape([4, 4, self.filters]),
-#                 PixelNorm(),
-#                 WSConv2D(in_channels=self.filters,
-#                          filters=self.filters,
-#                          kernel_size=3),
-#                 PixelNorm()
-#             ]
-#         )
-#         ## Initial RGB layer
-#         self.initial_rgb = rgb_conv(kernel_size=1)
-#         self.prog_block = [tf.keras.layers.InputLayer([latent_dim]), self.initial_layers]
-#         self.resolution: int = resolution_of_stage(self.stage)
-#
-#     @staticmethod
-#     def fade_in(upscaled, generated, alpha = 0.5, ):
-#         return tf.nn.tanh(alpha * generated + (1 - alpha) * upscaled)
-#
-#     def call(self, inputs, training=None, mask=None):
-#         print("def call was called inside the Generator Class")
-#         x = tf.nn.l2_normalize(inputs, axis=-1) if self.normalize_latents else inputs
-#         for layer in self.prog_block:
-#             x = layer(x)
-#         input_shape = x.shape
-#         if self.stage == 0:
-#             x = tf.keras.layers.Conv2D(3, (1, 1), activation=tf.keras.activations.relu, input_shape=input_shape)(x)
-#             return x
-#         else:
-#             for step in range(self.stage):
-#                 upscale_x = Upscale2D()(x)
-#                 x = self.prog_block[step](upscaled)
-#             final_upscaled = tf.keras.layers.Conv2D(3, (1, 1), activation=tf.keras.activations.relu, input_shape=input_shape)(x)
-#             return self.fade_in(alpha, final_upscaled, final_out)
-#
-#     def grow(self):
-#         self.stage += 1
-#         self.resolution *= 2
-#         print(f"Grow was called inside the Generator: \n"
-#               f"Generator now generating images of resolution {self.resolution} x {self.resolution}")
-#         generator_block = GeneratorBlock(self.stage)
-#         self.prog_block.append(generator_block)
-#         print(self.prog_block)
+        print(self.prog_block)
 
 
 # The Discriminator block
-def DiscriminatorBlock(stage, kernel_size=3, strides=1, padding = "same"):
+def DiscriminatorBlock(stage, kernel_size = 3, strides=1, padding = "same"):
     discriminator_block = tf.keras.models.Sequential(
         [
-            WSConv2D(in_channels=num_filters(stage+1),
-                     filters=num_filters(stage + 1),
-                     kernel_size=kernel_size,
-                     strides=strides,
-                     padding=padding),
-            WSConv2D(in_channels=num_filters(stage),
-                     filters=num_filters(stage),
-                     kernel_size=kernel_size,
-                     strides=strides,
-                     padding=padding),
+            standard_conv(filters=num_filters(stage + 1),
+                          kernel_size=kernel_size,
+                          strides=strides,
+                          padding=padding),
+            standard_conv(filters=num_filters(stage),
+                          kernel_size=kernel_size,
+                          strides=strides,
+                          padding=padding),
             tf.keras.layers.AveragePooling2D()
         ]
     )
     return discriminator_block
 
 
-## The Minibatch Standard Deviation Layer
-class MinibatchStdDevLayer(tf.keras.layers.Layer):
-    def __init__(self, group_size=4, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.group_size = group_size
-
-    def call(self, inputs, **kwargs):
-        x = NHWC_to_NCHW(inputs)
-        # Minibatch must be divisible by (or smaller than) group_size.
-        group_size = tf.minimum(self.group_size, tf.shape(x)[0])
-        # [NCHW]  Input shape.
-        s = x.shape
-        # [GMCHW] Split minibatch into M groups of size G.
-        y = tf.reshape(x, [group_size, -1, s[1], s[2], s[3]])
-        # [GMCHW] Cast to FP32.
-        y = tf.cast(y, tf.float32)
-        # [GMCHW] Subtract mean over group.
-        y -= tf.reduce_mean(y, axis=0, keepdims=True)
-        # [MCHW]  Calc variance over group.
-        y = tf.reduce_mean(tf.square(y), axis=0)
-        # [MCHW]  Calc stddev over group.
-        y = tf.sqrt(y + 1e-8)
-        # [M111]  Take average over fmaps and pixels.
-        y = tf.reduce_mean(y, axis=[1, 2, 3], keepdims=True)
-        # [M111]  Cast back to original data type.
-        y = tf.cast(y, x.dtype)
-        # [N1HW]  Replicate over group and pixels.
-        y = tf.tile(y, [group_size, 1, s[2], s[3]])
-        # [NCHW]  Append as new fmap.
-        z = tf.concat([x, y], axis=1)
-        ## Convert it back to NHWC
-        z_converted = NCHW_to_NHWC(z)
-        return z_converted
-
-
 # ### TODO: Make the Discriminator Block
 # ### TODO: 1) Implement the mixing factor for the discriminator
+# ### TODO: 2) Implement the Minibatch Standard Deviation Layer in the discriminator
 class Discriminator(tf.keras.models.Sequential):
     def __init__(self, resolution: int = 4, *args, **kwargs):
         super(Discriminator, self).__init__(*args, **kwargs)
@@ -317,15 +133,11 @@ class Discriminator(tf.keras.models.Sequential):
             d1 = DiscriminatorBlock(i)
             self.disc_block.append(d1)
 
-        ## Adding the Minibatch Standard Deviation Layer
         self.disc_block.append(
-            MinibatchStdDevLayer()
+            standard_conv(filters=num_filters(1), kernel_size=3)
         )
         self.disc_block.append(
-            WSConv2D(in_channels=num_filters(1)+1, filters=num_filters(1), kernel_size=3)
-        )
-        self.disc_block.append(
-            WSConv2D(in_channels=num_filters(1)+1, filters=num_filters(1), kernel_size=4, padding="valid")
+            standard_conv(filters=num_filters(1), kernel_size=4, padding="valid")
         )
         self.disc_block.append(
             tf.keras.layers.Flatten()
@@ -346,6 +158,7 @@ class Discriminator(tf.keras.models.Sequential):
         print("Grow was called inside the discriminator "
               f"Discriminator now inputting images of resolution {self.resolution}x{self.resolution}")
         new = Discriminator(resolution=self.resolution*2)
+        print(f"Resolution after growing {new.resolution}")
         current_layers = {
             l.name: l for l in self.layers
         }
@@ -360,7 +173,7 @@ class Discriminator(tf.keras.models.Sequential):
 # noise = tf.random.normal([1, 1, 1, 128])
 # gen_1 = Generator()
 # out = gen_1(noise)
-# print(out)
+# print(out.shape)
 # print(gen_1.resolution)
 # for _ in range(8):
 #     gen_1.grow()
@@ -392,31 +205,14 @@ class Discriminator(tf.keras.models.Sequential):
 # print(f"Discriminator resolution: {disc.resolution}")
 # for layer in disc.layers:
 #     print(layer)
-#
-#
+
+
 # gen.grow()
-# out2 = gen(noise)
+# print(f"Resolution the generator is now outputting: {gen.resolution}")
+# print(f"Stage of the resolution of the generator: {gen.stage}")
 # disc1 = disc.grow()
 # print("Discriminator has grown")
 # print(f"Discriminator stage: {disc1.stage}")
 # print(f"Discriminator resolution: {disc1.resolution}")
-# y1 = disc1(out2)
 # for layer in disc1.layers:
 #     print(layer)
-
-
-### Testing sequence for the Minibatch Standard Deviation:
-# noise = tf.random.normal([1, 512])
-# gen = Generator()
-# out = gen(noise)
-# print(f"output shape: {out.shape}")
-# print(f"Resolution of the Generator:{gen.resolution}")
-# minibatch_std = MinibatchStdDevLayer()(out)
-# print(f"Output shape of Minibatch Standard Deviation layer {minibatch_std.shape}")
-
-
-### Testing sequence for upscale layer
-# noise = tf.random.normal([1, 4, 4, 3])
-# x = Upscale2D()(noise)
-# print(f"Noise shape: {noise.shape}")
-# print(f"Upscaled noise shape: {x.shape}")
